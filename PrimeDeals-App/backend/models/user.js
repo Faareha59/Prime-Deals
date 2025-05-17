c4f643ffb80617229f4dfcc5daa8bd5038ed13b5
maham535 <bibimaham322@gmail.com>
2025-05-17 13:34:42 +0500

    Update user.js

--- PrimeDeals-App/backend/models/user.js
+++ PrimeDeals-App/backend/models/user.js
@@ -1,21 +1,14 @@
 // backend/models/user.js
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  username: {
-    type: String,
-    required: true,
-    unique: true, // Ensure usernames are unique
-  },
-  password: {
-    type: String,
-    required: true,
-  }
+    agent: String,
+    description: String,
+    features: String,
 });
+
+// Explicitly set the collection name to 'properties'
+const PropertiesModel = mongoose.model('Properties', propertiesSchema, 'properties');
+module.exports = PropertiesModel;
 
 const user = mongoose.model('User', userSchema); // Create the model
 
